f20ecc8d390aa4834708f6504c36acf7bb3c11eb
MAGA <marcogall@outlook.de>
2024-01-05 22:19:06 +0100

    Azure Key Vault

--- Aspire/Aspire.AppHost/Program.cs
+++ Aspire/Aspire.AppHost/Program.cs
@@ -1,13 +1,11 @@
-using System.Diagnostics;
 using TIK.Shared.Helper;
-using Aspire.Azure.Security.KeyVault;
-using Microsoft.Extensions.Hosting; 
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 var keyVault = builder.AddAzureKeyVault("secrets");
 
- var backend = builder.AddProject<Projects.TIK_Backend>(Constants.BACKEND)
+
+var backend = builder.AddProject<Projects.TIK_Backend>(Constants.BACKEND)
      .WithReference(keyVault);
 
 builder.AddProject<Projects.TIK_Frontend_Server>(Constants.FRONTEND)
--- TIK.Backend/Controllers/WeatherForecastController.cs
+++ TIK.Backend/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ using TIK.Shared;
 namespace TIK.Backend.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
 
